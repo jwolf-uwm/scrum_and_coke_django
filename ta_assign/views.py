66d1cf30fbc3170e660c2f75977509c4d9c55bfd
MHibbard77 <hibbard3@uwm.edu>
2019-03-29 16:54:15 -0500

    Views

--- ta_assign/views.py
+++ ta_assign/views.py
@@ -6,15 +6,14 @@ from ta_assign.models import Person
 
 class Home(View):
 
-   def get(self,request):
-    return render(request, 'main/index.html')
+    def get(self,request):
+        return render(request, 'main/index.html')
 
-
-  def post(self,request):
-      your_instance = Person()
-      command_input = request.POST["command"]
-      if command_input:
-        response = your_instance.command(command_input)
-      else:
-        response = ""
-      return render(request, 'main/index.html',{"message":response})
+    def post(self,request):
+        your_instance = Person()
+        command_input = request.POST["command"]
+        if command_input:
+            response = your_instance.command(command_input)
+        else:
+            response = ""
+        return render(request, 'main/index.html',{"message":response})
