5d30cc3655e743a95c8d37a48bd1060b9fdec15e
evanv <evetrano@uwm.edu>
2019-03-29 15:25:24 -0500

    nude models

--- ta_assign/models.py
+++ ta_assign/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Person(models.Model):
+    email = models.CharField()
+    password = models.CharField()
+    phone = models.IntField(default=-1)
+    name = models.CharField(default="DEFAULT")
+    isLoggedOn = models.BoolField(default=False)
+
+
+class Administrator(Person):
+    def __init__(self):
+        super()
+
+
+class Supervisor(Person):
+    def __init__(self):
+        super()
