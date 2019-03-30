b6d0a61434c4e7c16ec58b353238ecb6ea4c8be2
Jeffrey Wolf <lwolf@uwm.edu>
2019-03-30 07:27:06 -0500

    adjusted view to repeat command as text

--- ta_assign/views.py
+++ ta_assign/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views import View
-from ta_assign.models import ModelPerson
 
 # Create your views here.
 
@@ -11,10 +10,11 @@ class Home(View):
         return render(request, 'main/index.html')
 
     def post(self, request):
-        your_instance = ModelPerson()
+        #your_instance = SomeCommandClass()
         command_input = request.POST["command"]
-        if command_input:
-            response = your_instance.command(command_input)
-        else:
-            response = ""
+        #if command_input:
+        #    response = your_instance.command(command_input)
+        #else:
+        #    response = ""
+        response = command_input
         return render(request, 'main/index.html', {"message": response})
