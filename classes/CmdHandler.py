c3662c7d3149f152a30f2b1eec05dd2f9b721461
evanv <evetrano@uwm.edu>
2019-04-02 12:56:38 -0500

    changed things and stuffs

--- classes/CmdHandler.py
+++ classes/CmdHandler.py
@@ -15,30 +15,51 @@ class CmdHandler:
     logged_in = False
 
     def check_setup(self):
-
-        find_admin = models.ModelAdministrator.objects.all()
-
-        if not find_admin.exists():
+        # check if an administrator exists
+        try:
+            models.ModelPerson.objects.get(type="administrator")
+        except models.ModelPerson.DoesNotExist:
             return False
-
-        else:
-            return True
+        return True
 
     def deal_with_command(self, some_cmd):
-
+        # if we don't have an admin yet
         if not self.check_setup():
-
+            # if we didn't type setup, tell them to setup
             if some_cmd != "setup":
                 return "Please run setup before attempting to execute commands."
-
+            # create a new admin
             else:
                 # do setup stuff here
-                parse_cmd = some_cmd.split()
-                if parse_cmd[0] == "create_course":
-                    if len(parse_cmd) != 3:
-                        return "Create course not of the right format: create_course course_id:String num_labs:Int"
-
                 return "This will set stuff up someday."
 
         else:
-            return "I'M HELPING LOOK: " + some_cmd
+            parse_cmd = some_cmd.split()
+            # check login
+            if not self.logged_in:
+                if parse_cmd[0] != "Login":
+                    return "You have to log in first dummy"
+                else:
+                    try:
+                        find_person = models.ModelPerson.objects.get(email=parse_cmd[1])
+                    except models.ModelPerson.DoesNotExist:
+                        return f"The user {parse_cmd[1]} is not in the database"
+                    if find_person.password != parse_cmd[2]:
+                        return "The entered password is incorrect"
+                    self.logged_in = True
+                    self.current_user = find_person
+                    return "Logged in successfully"
+
+            # check logout
+            if parse_cmd[0] == "Logout":
+                self.logged_in = False
+                return "Screw you guys, I'm going home"
+
+            # check create_course
+            if parse_cmd[0] == "create_course":
+                if self.current_user.type != "administrator" or self.current_user.type != "supervisor":
+                    return "Yeah, you don't have access to that command. Nice try buddy."
+                if len(parse_cmd) != 3:
+                    return "Create course not of the right format: create_course course_id num_labs"
+
+            return "I don't even know what the heck you just wrote. Do it again but better."
