ca0a9802454e9b4f09e7c598af984f7eedbb2abf
MHibbard77 <hibbard3@uwm.edu>
2019-04-02 10:10:56 -0500

    Merge branch 'master' of https://github.com/superransom/scrum_and_coke_django into matts_branch
    # Conflicts:
    #	ta_assign/views.py

---
+++ ta_assign/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views import View
+from classes.CmdHandler import CmdHandler
+
+# Create your views here.
+
+
+class Home(View):
+
+    def get(self,request):
+        return render(request, 'main/index.html')
+
+    def post(self, request):
+        get_workin = CmdHandler()
+        command_input = request.POST["command"]
+        if command_input:
+            response = get_workin.deal_with_command(command_input)
+        else:
+            response = "Please type a command to do stuff."
+
+        return render(request, 'main/index.html', {"message": response})
+
