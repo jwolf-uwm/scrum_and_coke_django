4e0f1c386eeb06511ce8461f3e3e47dd848a724c
MHibbard77 <hibbard3@uwm.edu>
2019-04-03 10:53:22 -0500

    Merge branch 'master' of https://github.com/superransom/scrum_and_coke_django into matts_branch
    # Conflicts:
    #	ta_assign/views.py

--- classes/Supervisor.py
+++ classes/Supervisor.py
@@ -22,13 +22,30 @@ class Supervisor(Administrator):
         """
         assigns the given instructor's course to the course parameter
         """
-        course_id.instructor = email
+        try:
+            find_course = models.ModelCourse.objects.get(course_id=course_id)
+        except models.ModelCourse.DoesNotExist:
+            find_course = "none"
+
+        if find_course != "none":
+            find_course.instructor = email
+        else:
+            print("No such course")
         return
 
     def assign_ta_course(self, email, course_id, course_section):
         """
         assigns the given TA's course to the course parameter
         """
+        try:
+            ta_course = models.ModelTACourseCourse.objects.get(course_id=course_id)
+        except models.ModelCourse.DoesNotExist:
+            ta_course = "none"
+
+        if ta_course != "none":
+            ta_course.TA
+        else:
+            print("No such course")
         course_id.tee_ays.append(email)
         return
 
