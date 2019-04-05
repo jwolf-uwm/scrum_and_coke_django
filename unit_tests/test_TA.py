db7341ec19d91e985f4e024db08f2cd22ad34e3b
smit2633 <smit2633@uwm.edu>
2019-04-05 18:43:50 -0500

    Committed by Dillan!

--- unit_tests/test_TA.py
+++ unit_tests/test_TA.py
@@ -20,6 +20,35 @@ class TestTA(TestCase):
         self.course2 = Course("CS201", 0)
         self.course3 = Course("CS301", 0)
         self.course4 = Course("CS401", 0)
+
+    def test_view_ta_assignments1(self):
+        mod_course1 = models.ModelCourse.objects.get(course_id="CS101")
+        mod_ta_course1 = models.ModelTACourse()
+        mod_ta_course1.course = mod_course1
+        mod_ta1 = models.ModelPerson.objects.get(email="ta1@uwm.edu")
+        mod_ta_course1.TA = mod_ta1
+        mod_ta_course1.save()
+        self.assertEqual(self.ta1.view_ta_assignments()[0], "Course: CS101 TA: DEFAULT, ta1@uwm.edu")
+
+    def test_view_ta_assignments2(self):
+        mod_course2 = models.ModelCourse.objects.get(course_id="CS201")
+        mod_ta_course2 = models.ModelTACourse()
+        mod_ta_course2.course = mod_course2
+        mod_ta2 = models.ModelPerson.objects.get(email="ta2@uwm.edu")
+        mod_ta_course2.TA = mod_ta2
+        mod_ta_course2.save()
+        self.assertEqual(self.ta1.view_ta_assignments()[0], "Course: CS201 TA: DEFAULT, ta2@uwm.edu")
+
+    def test_view_ta_assignments3(self):
+        mod_course3 = models.ModelCourse.objects.get(course_id="CS301")
+        mod_ta_course3 = models.ModelTACourse()
+        mod_ta_course3.course = mod_course3
+        mod_ta3 = models.ModelPerson.objects.get(email="ta3@uwm.edu")
+        mod_ta_course3.TA = mod_ta3
+        mod_ta_course3.save()
+        self.assertEqual(self.ta1.view_ta_assignments()[0], "Course: CS301 TA: DEFAULT, ta3@uwm.edu")
+
+    def test_view_ta_assignments4(self):
         mod_course1 = models.ModelCourse.objects.get(course_id="CS101")
         mod_ta_course1 = models.ModelTACourse()
         mod_ta_course1.course = mod_course1
@@ -38,8 +67,6 @@ class TestTA(TestCase):
         mod_ta3 = models.ModelPerson.objects.get(email="ta3@uwm.edu")
         mod_ta_course3.TA = mod_ta3
         mod_ta_course3.save()
-
-    def test_view_ta_assignments(self):
         self.assertEqual(self.ta1.view_ta_assignments()[0], "Course: CS101 TA: DEFAULT, ta1@uwm.edu")
         self.assertEqual(self.ta1.view_ta_assignments()[1], "Course: CS201 TA: DEFAULT, ta2@uwm.edu")
         self.assertEqual(self.ta1.view_ta_assignments()[2], "Course: CS301 TA: DEFAULT, ta3@uwm.edu")
@@ -50,5 +77,3 @@ class TestTA(TestCase):
     def test_read_public_contact(self):
         # self.assertEqual(self.ta1.read_public_contact(self.class_list))
         pass
-
-    models.ModelPerson.objects.all().delete()
