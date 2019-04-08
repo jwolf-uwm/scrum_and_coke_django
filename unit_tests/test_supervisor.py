5f459f860dc7cb9609991c1b80158194d21c6ccc
evanv <evetrano@uwm.edu>
2019-04-08 12:56:19 -0500

    all the tests

--- unit_tests/test_supervisor.py
+++ unit_tests/test_supervisor.py
@@ -1,12 +1,12 @@
-# created by Evan
-
+# created by Matt
 from django.test import TestCase
+from classes.Person import Person
+from classes.Administrator import Administrator
 from classes.Supervisor import Supervisor
-from ta_assign import models
-from classes.Course import Course
 from classes.Instructor import Instructor
 from classes.TA import TA
-from classes.Administrator import Administrator
+from ta_assign import models
+from classes.Course import Course
 
 
 class TestSupervisor(TestCase):
@@ -135,6 +135,59 @@ class TestSupervisor(TestCase):
 
         self.assertRaises(self.sup1.assign_ta_lab(self.ins1, self.course1[0]), TypeError)
 
+    def test_create_course_as_supervisor(self):
+        # create a new course as admin
+        self.assertTrue(self.sup1.create_course("CS361-401", 3))
+        # get the added course from the db
+        da_course = models.ModelCourse.objects.get(course_id="CS361-401")
+        # make sure found course is the same
+        self.assertEqual(da_course.course_id, "CS361-401")
+        self.assertEqual(da_course.num_labs, 3)
+        self.assertEqual(da_course.instructor, "not_set@uwm.edu")
+
+    def test_create_course_again(self):
+        self.assertTrue(self.sup1.create_course("CS361-401", 3))
+        # create the same course again with no changes
+        self.assertFalse(self.sup1.create_course("CS361-401", 3))
+        # create the same course with a different number of labs
+        self.assertFalse(self.sup1.create_course("CS361-401", 2))
+        # create the same course with a different section number (technically a new course)
+        self.assertTrue(self.sup1.create_course("CS361-402", 3))
+        da_course = models.ModelCourse.objects.get(course_id="CS361-402")
+        # make sure found course is the same
+        self.assertEqual(da_course.course_id, "CS361-402")
+        self.assertEqual(da_course.num_labs, 3)
+        self.assertEqual(da_course.instructor, "not_set@uwm.edu")
+
+    def test_create_course_missing_parameters(self):
+        with self.assertRaises(TypeError):
+            self.sup1.create_course("CS101-401")
+        # missing course_id/wrong type
+        with self.assertRaises(TypeError):
+            self.sup1.create_course(3)
+
+    def test_create_course_long_course_id(self):
+        # course_id too long and not right format
+        self.assertFalse(self.sup1.create_course("totally_a_good_course_id", 2))
+
+    def test_create_course_course_id_incorrect(self):
+        # course_id missing CS at beginning
+        self.assertFalse(self.sup1.create_course("123456789", 2))
+        # course_id does not start with uppercase CS
+        self.assertFalse(self.sup1.create_course("cs361-401", 2))
+        # course_id doesn't have only numbers for course number
+        self.assertFalse(self.sup1.create_course("CS3F4-321", 2))
+        # course_id doesn't have a hyphen to separate course number and section number
+        self.assertFalse(self.sup1.create_course("CS3611234", 2))
+        # course_id doesn't have only numbers for section number
+        self.assertFalse(self.sup1.create_course("CS361-1F3", 2))
+
+    def test_create_course_bad_num_sections(self):
+        # number of sections too big
+        self.assertFalse(self.sup1.create_course("CS361-401", 99))
+        # number of sections is less than 0
+        self.assertFalse(self.sup1.create_course("CS361-401", -1))
+
     # Create Account Tests Start
     # created by Jeff
     def test_create_account_instructor(self):
@@ -242,6 +295,77 @@ class TestSupervisor(TestCase):
         self.assertFalse(self.sup1.create_account("FredClaus@uwm.edu", "santa_bro", "ta"))
 
     # Create Account Tests End
+
+    def test_edit_account_password(self):
+        # create a test user in the system
+        tester = models.ModelPerson()
+        tester.email = "rando@uwm.edu"
+        tester.password = "random_password"
+        tester.save()
+        # test edit password
+        self.sup1.edit_account("rando@uwm.edu", "password", "new_pass")
+
+        tester = models.ModelPerson.objects.get(email="rando@uwm.edu")
+        self.assertEqual(tester.password, "new_pass")
+
+    def test_edit_account_email(self):
+        # create a test user in the system
+        tester = models.ModelPerson()
+        tester.email = "rando@uwm.edu"
+        tester.password = "random_password"
+        tester.save()
+        # test edit email
+        self.sup1.edit_account("rando@uwm.edu", "email", "NEW_EMAIL@uwm.edu")
+
+        with self.assertRaises(models.ModelPerson.DoesNotExist):
+            models.ModelPerson.objects.get(email="rando@uwm.edu")
+        tester = models.ModelPerson.objects.get(email="NEW_EMAIL@uwm.edu")
+        self.assertEqual(tester.email, "NEW_EMAIL@uwm.edu")
+        self.assertFalse(self.sup1.edit_account("rando@uwm.edu", "email", "badEmail@uwm.edu@uwm.edu"))
+        self.assertFalse(self.sup1.edit_account("rando@uwm.edu", "email", "badEmail@gmail.com"))
+        self.assertFalse(self.sup1.edit_account("rando@uwm.edu", "email", "badEmail"))
+
+    def test_edit_account_phone(self):
+        # create a test user in the system
+        tester = models.ModelPerson()
+        tester.email = "rando@uwm.edu"
+        tester.password = "random_password"
+        tester.save()
+        # test edit phone
+        self.assertTrue(self.sup1.edit_account("rando@uwm.edu", "phone_number", "123.456.7890"))
+
+        tester = models.ModelPerson.objects.get(email="rando@uwm.edu")
+        self.assertEqual(tester.phone, "123.456.7890")
+        self.assertFalse(self.sup1.edit_account("rando@uwm.edu", "phone_number", "not a number"))
+        self.assertFalse(self.sup1.edit_account("rando@uwm.edu", "phone_number", "1234"))
+        self.assertFalse(self.sup1.edit_account("rando@uwm.edu", "phone_number", "1234567890987654"))
+        self.assertFalse(self.sup1.edit_account("rando@uwm.edu", "phone_number", "414-414-4141"))
+        self.assertFalse(self.sup1.edit_account("rando@uwm.edu", "phone_number", "(414)414-4141"))
+        self.assertFalse(self.sup1.edit_account("rando@uwm.edu", "phone_number", "abc.abc.abcd"))
+        self.assertFalse(self.sup1.edit_account("rando@uwm.edu", "phone_number", "1234.1234.1234"))
+
+    def test_edit_account_name(self):
+        # create a test user in the system
+        tester = models.ModelPerson()
+        tester.email = "rando@uwm.edu"
+        tester.password = "random_password"
+        tester.save()
+        # test edit name
+        self.sup1.edit_account("rando@uwm.edu", "name", "Howard Stern")
+
+        tester = models.ModelPerson.objects.get(email="rando@uwm.edu")
+        self.assertEqual(tester.name, "Howard Stern")
+        self.assertFalse(self.sup1.edit_account("wrong_email@uwm.edu", "password", "new_pass"))
+        self.assertFalse(self.sup1.edit_account("rando@uwm.edu", "wrong_field", "new_pass"))
+
+    def test_delete_account(self):
+        self.deleted_user = ("delete_me@uwm.edu", "delete_me_pass")
+        self.sup1.delete_account("delete_me@uwm.edu")
+        self.copy_user = ("delete_me@uwm.edu", "delete_me_pass")
+        self.assertNotEqual(self.copy_user, self.deleted_user)
+
+    def test_send_notification(self):
+        self.assertTrue(self.sup1.send_notification("I Like To Eat French Fries In The Rain"))
 
     # Access Info Tests Start
     # Jeff's tests
@@ -396,5 +520,58 @@ class TestSupervisor(TestCase):
         self.assertEqual(parse_info[18], "Courses:")
         self.assertEqual(parse_info[19], "CS101")
         self.assertEqual(parse_info[20], "CS102")
-
     # Access Info Tests End
+    # Person tests
+    def test_init_(self):
+        self.Supervisor1 = Supervisor("Supervisor1@uwm.edu", "DEFAULT_PASSWORD", "DEFAULT")
+        self.assertEquals(self.Supervisor1.email, "Supervisor1@uwm.edu")
+        self.assertEquals(self.Supervisor1.password, "DEFAULT_PASSWORD")
+
+    def test_change_password(self):
+        self.Supervisor1 = Supervisor("Supervisor1@uwm.edu", "DEFAULT_PASSWORD", "DEFAULT")
+        self.assertTrue(self.Supervisor1.change_password("password"))
+        self.assertEquals(self.Supervisor1.password, "password")
+        self.assertNotEquals(self.Supervisor1.password, "DEFAULT_PASSWORD")
+
+    def test_change_email(self):
+        self.Supervisor1 = Supervisor("Supervisor1@uwm.edu", "DEFAULT_PASSWORD", "DEFAULT")
+        self.Supervisor1.change_email("snoop@uwm.edu")
+        self.assertEquals(self.Supervisor1.email, "snoop@uwm.edu")
+        self.assertNotEquals(self.Supervisor1.email, "Supervisor1@uwm.edu")
+        self.assertFalse(self.Supervisor1.change_email("snoop@gmail.com"))
+        self.assertFalse(self.Supervisor1.change_email("no_at_symbol_or_dot_something"))
+
+    def test_change_phone(self):
+        self.Supervisor1 = Supervisor("Supervisor1@uwm.edu", "DEFAULT_PASSWORD", "DEFAULT")
+        self.Supervisor1.change_phone("414.414.4141")
+        self.assertEquals(self.Supervisor1.phone_number, "414.414.4141")
+        self.assertNotEquals(self.Supervisor1.phone_number, "000.000.0000")
+        self.assertFalse(self.Supervisor1.change_phone("1234567890"))
+        self.assertFalse(self.Supervisor1.change_phone("414-414-4141"))
+        self.assertFalse(self.Supervisor1.change_phone("(414)414-4141"))
+        self.assertFalse(self.Supervisor1.change_phone("abc.abc.abcd"))
+        self.assertFalse(self.Supervisor1.change_phone("1234.1234.1234"))
+
+    def test_change_name(self):
+        self.Supervisor1 = Supervisor("Supervisor1@uwm.edu", "DEFAULT_PASSWORD", "DEFAULT")
+        self.Supervisor1.change_name("Snoop Doggy Dog")
+        self.assertEquals(self.Supervisor1.name, "Snoop Doggy Dog")
+        self.assertNotEquals(self.Supervisor1.name, "DEFAULT")
+
+    def test_get_contact_info(self):
+        self.Supervisor1 = Supervisor("Supervisor1@uwm.edu", "DEFAULT_PASSWORD", "DEFAULT")
+        self.assertEquals(self.Supervisor1.get_contact_info(), "Snoop Doggy Dog, snoop@uwm.edu, 4144244343")
+        self.assertNotEquals(self.Supervisor1.get_contact_info(), "DEFAULT, Supervisor1@uwm.edu, 0000000000")
+
+    def test_login(self):
+        self.Supervisor1 = Supervisor("Supervisor1@uwm.edu", "DEFAULT_PASSWORD", "DEFAULT")
+        self.assertEquals(Supervisor.login("Supervisor1@uwm.edu", "DEFAULT_PASSWORD"), "Login successful")
+        model_supervisor1 = models.ModelPerson.objects.get(email=self.Supervisor1.email)
+        self.assertTrue(model_supervisor1.isLoggedOn)
+        self.assertEquals(Supervisor.login("Supervisor1@uwm.edu", "DEFAULT_PASSWORD"), "User already logged in")
+        self.assertEquals(Supervisor.login("snoop@uwm.edu", "password"), "Invalid login info")
+
+    def test_logout(self):
+        self.Supervisor1 = Supervisor("Supervisor1@uwm.edu", "DEFAULT_PASSWORD", "DEFAULT")
+        self.assertEquals(Supervisor.login("Supervisor1@uwm.edu", "DEFAULT_PASSWORD"), "Login successful")
+        self.assertTrue(self.Supervisor1.logout())
