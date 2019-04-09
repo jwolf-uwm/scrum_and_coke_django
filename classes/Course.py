cff18cc598037fbf2230baf00e8a58f17e79dd3a
smit2633 <smit2633@uwm.edu>
2019-04-08 20:30:56 -0500

    Committed my Dillan.

---
+++ classes/Course.py
@@ -0,0 +1,47 @@
+from ta_assign import models
+
+
+class Course:
+    def __init__(self, course_id, num_labs):
+        self.course_id = course_id
+        self.num_labs = num_labs
+        # instructor should be a class at some point
+        self.instructor = "not_set@uwm.edu"
+        self.tee_ays = []
+
+        some_course = models.ModelCourse()
+        some_course.course_id = self.course_id
+        some_course.num_labs = self.num_labs
+        some_course.instructor = self.instructor
+        # don't think there's any need to port over an empty list
+        some_course.save()
+
+    def set_course_id(self, course_id):
+        return
+
+    def set_course_section(self, section_number):
+        return
+
+    def set_instructor(self, instructor):
+        return
+
+    def set_num_labs(self, lab_section):
+        return
+
+    def add_tee_ay(self, tee_ay):
+        return
+
+    def get_course_id(self):
+        return
+
+    def get_course_name(self):
+        return
+
+    def get_course_section(self):
+        return
+
+    def get_num_labs(self):
+        return
+
+    def get_tee_ays(self):
+        return
---
+++ classes/Person.py
@@ -0,0 +1,89 @@
+# created by Grant
+from ta_assign import models
+
+
+class Person:
+    def __init__(self, email, password, account_type):
+        self.email = email
+        self.password = password
+        self.phone_number = -1
+        self.name = "DEFAULT"
+        self.type = account_type
+        self.isLoggedOn = False
+
+        try:
+            find_email = models.ModelPerson.objects.get(email=email)
+        except models.ModelPerson.DoesNotExist:
+            find_email = "none"
+
+        if find_email == "none":
+            some_guy = models.ModelPerson()
+            some_guy.email = self.email
+            some_guy.password = self.password
+            some_guy.name = self.name
+            some_guy.phone = self.phone_number
+            some_guy.type = self.type
+            some_guy.isLoggedOn = self.isLoggedOn
+            some_guy.save()
+
+    def change_password(self, new):
+        self.password = new
+        return True
+
+    def change_email(self, address):
+        parse_at = address.split("@")
+
+        try:
+            if len(parse_at) != 2 or parse_at[1] != "uwm.edu":
+                return False
+        except ValueError:
+            return False
+
+        self.email = address
+        return True
+
+    def change_name(self, name):
+        self.name = name
+        return True
+
+    def change_phone(self, phone):
+        if not isinstance(phone, int):
+            print("phone number contains illegal characters")
+            return False
+        elif int(phone/1000000000) >= 10 or int(phone/1000000000) <= 0:
+            print("phone number is the wrong length")
+            return False
+        else:
+            self.phone_number = phone
+        return True
+
+    def get_contact_info(self):
+        return
+
+    @staticmethod
+    def login(email, password):
+
+        people = models.ModelPerson.objects.all()
+
+        temp = None
+
+        for i in people:
+            if i.email == email:
+                temp = i
+
+        if temp is None:
+            return "Invalid login info"
+        elif temp.email != email or temp.password != password:
+            return "Invalid login info"
+        if temp.isLoggedOn is True:
+            return "User already logged in"
+        models.ModelPerson.objects.filter(email=email).update(isLoggedOn=True)
+        return "Login successful"
+
+    def logout(self):
+
+        person = models.ModelPerson.objects.get(email=self.email)
+        if person.isLoggedOn is False:
+            return False
+        models.ModelPerson.objects.filter(email = self.email).update(isLoggedOn=False)
+        return True
