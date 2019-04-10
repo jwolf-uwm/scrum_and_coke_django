4e1edbb6123be227bb935683855b2b83d8ce8138
Jeffrey Wolf <lwolf@uwm.edu>
2019-04-09 20:21:27 -0500

    dealing with access_info acceptance

--- acceptance/access_info.py
+++ acceptance/access_info.py
@@ -15,28 +15,41 @@ class CreateAccountTests(TestCase):
         self.assertEqual(self.ui.parse_command("create_account ta@uwm.edu password ta"),
                          "Please login first.")
 
+    def test_command_access_info_admin(self):
+        self.ui.parse_command("setup")
+        self.ui.parse_command("login ta_assign_admin@uwm.edu password")
+        big_string = self.ui.parse_command("access_info")
+        parse_string = big_string.split("\n")
+        self.assertEqual(parse_string[0], "Administrator:")
+        self.assertEqual(parse_string[1], "DEFAULT | ta_assign_admin@uwm.edu | 000.000.0000")
+        self.assertEqual(parse_string[2], "")
+        self.assertEqual(parse_string[3], "Supervisor:")
+        self.assertEqual(parse_string[4], "DEFAULT | ta_assign_super@uwm.edu | 000.000.0000")
 
+    def test_command_access_info_super(self):
+        self.ui.parse_command("setup")
+        self.ui.parse_command("login ta_assign_super@uwm.edu password")
+        big_string = self.ui.parse_command("access_info")
+        parse_string = big_string.split("\n")
+        self.assertEqual(parse_string[0], "Administrator:")
+        self.assertEqual(parse_string[1], "DEFAULT | ta_assign_admin@uwm.edu | 000.000.0000")
+        self.assertEqual(parse_string[2], "")
+        self.assertEqual(parse_string[3], "Supervisor:")
+        self.assertEqual(parse_string[4], "DEFAULT | ta_assign_super@uwm.edu | 000.000.0000")
 
-    def test_valid_access_admin(self):
-        self.ui.command("Login admin@uwm.edu adminPass")
-        self.assertEqual(self.ui.command("AccessInfo"), "NAME           PHONE           EMAIL           PASSWORD"
-                                                        "Dave Brubeck   414.123.4567    admin@uwm.edu   adminPass"
-                                                        "Donna Summer   414.987.6543    super@uwm.edu   superPass"
-                                                        "Dean Martin    262.123.4567    instr@uwm.edu   instrPass"
-                                                        "Daniel Craig   262.987.6543    t_ayy@uwm.edu   t_ayyPass")
+    def test_command_access_info_instructor(self):
+        self.ui.parse_command("setup")
+        self.ui.parse_command("login ta_assign_admin@uwm.edu password")
+        self.ui.parse_command("create_account instructor@uwm.edu password instructor")
+        self.ui.parse_command("logout")
+        self.ui.parse_command("login instructor@uwm.edu password")
+        self.assertEqual(self.ui.parse_command("access_info"), "Invalid command.")
 
-    def test_valid_access_super(self):
-        self.ui.command("Login super@uwm.edu superPass")
-        self.assertEqual(self.ui.command("AccessInfo"), "NAME           PHONE           EMAIL           PASSWORD"
-                                                        "Dave Brubeck   414.123.4567    admin@uwm.edu   adminPass"
-                                                        "Donna Summer   414.987.6543    super@uwm.edu   superPass"
-                                                        "Dean Martin    262.123.4567    instr@uwm.edu   instrPass"
-                                                        "Daniel Craig   262.987.6543    t_ayy@uwm.edu   t_ayyPass")
+    def test_command_access_info_ta(self):
+        self.ui.parse_command("setup")
+        self.ui.parse_command("login ta_assign_admin@uwm.edu password")
+        self.ui.parse_command("create_account ta@uwm.edu password ta")
+        self.ui.parse_command("logout")
+        self.ui.parse_command("login ta@uwm.edu password")
+        self.assertEqual(self.ui.parse_command("access_info"), "Invalid command.")
 
-    def test_invalid_access_instr(self):
-        self.ui.command("Login instr@uwm.edu instrPass")
-        self.assertEqual(self.ui.command("AccessInfo"), "Access Denied")
-
-    def test_invalid_access_ta(self):
-        self.ui.command("Login t_ayy@uwm.edu t_ayyPass")
-        self.assertEqual(self.ui.command("AccessInfo"), "Access Denied")
