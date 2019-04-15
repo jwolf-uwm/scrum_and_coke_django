05922b4c8c8f141194071808962ccb8868f08a86
Jeffrey Wolf <lwolf@uwm.edu>
2019-04-14 21:19:16 -0500

    start of createaccount page

--- ta_assign/views.py
+++ ta_assign/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from classes.CmdHandler import CmdHandler
+from classes.Administrator import Administrator
 
 # Create your views here.
 
 
 class Home(View):
-    def get(self,request):
+    def get(self, request):
         return render(request, 'main/index.html')
 
     def post(self, request):
@@ -18,3 +19,12 @@ class Home(View):
             response = "Please type a command to do stuff."
 
         return render(request, 'main/index.html', {"message": response})
+
+
+class CreateAccount(View):
+    def get(self, request):
+        return render(request, 'create_account.html')
+
+    def post(self, request):
+        # TBD
+        return
