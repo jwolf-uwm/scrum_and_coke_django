513b9a9ebedfeea7426e69db0be94c7793d980d7
Jeffrey Wolf <lwolf@uwm.edu>
2019-04-21 13:28:29 -0500

    first access_info acceptance wooooo

--- django_acceptance/test_access_info.py
+++ django_acceptance/test_access_info.py
@@ -8,3 +8,15 @@ class CreateAccessInfo(TestCase):
 
     def setUp(self):
         return
+
+    def test_no_login_get(self):
+
+        client = Client()
+        response = client.get('/access_info/')
+        # this gets any messages
+        all_messages = [msg for msg in get_messages(response.wsgi_request)]
+        # this should be the first and only message, tagged error
+        self.assertEqual(all_messages[0].tags, "error")
+        self.assertEqual(all_messages[0].message, "Please login first.")
+        # since we returned a redirect, we can check the location
+        self.assertEqual(response.get('location'), '/login/')
