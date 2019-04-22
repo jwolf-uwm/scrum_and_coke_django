c3a7badd0882d84d5859306b29cfc5d66dadfce9
MHibbard77 <hibbard3@uwm.edu>
2019-04-21 23:12:39 -0500

    acceptance tests for assign_instructors

--- ta_assign/views.py
+++ ta_assign/views.py
@@ -42,3 +42,11 @@ class CreateCourse(View):
         num_labs = request.POST["num_labs"]
         return render(request, 'main/create_course.html', {"message": [course_id, course_section, num_labs]})
 
+class AssignInstructor(View):
+    def get(self, request):
+        return render(request, 'main/assign_instructor.html')
+
+    def post(self, request):
+        course_id = request.POST["course_id"]
+        email = request.POST["Instructor email"]
+        return render(request, 'main/assign_instructor.html', {"message": [course_id, email]})
